b9c2cd4af3e6d21ddadf8bc1841cf8271d02357b
RustamOper05 <sahabutdinov200@gmail.com>
2024-02-20 16:57:14 +0300

    add index controller&view

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const app = await NestFactory.create(AppModule);
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('pug');
+
   await app.listen(port);
 }
 bootstrap();
